a4efd4124aec28e2fcfe8fb07c8bbb65519b6ca3
richimenendez <richimenen@gmail.com>
2020-10-03 03:09:59 -0600

    test cpu3

--- ServerL/Procs/Cpu/cpu-module.c
+++ ServerL/Procs/Cpu/cpu-module.c
@@ -1,4 +1,3 @@
-/*
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/proc_fs.h>
@@ -91,42 +90,4 @@ static void __exit test_exit(void)
 
 module_init(test_init);
 module_exit(test_exit);
-MODULE_LICENSE("GPL");
-*/
-// SPDX-License-Identifier: GPL-3.0
-#include <linux/kernel.h>  // printk(), pr_*()
-#include <linux/module.h>  // THIS_MODULE, MODULE_VERSION, ...
-#include <linux/init.h>    // module_{init,exit}()
-#include <linux/smp.h>     // get_cpu(), put_cpu()
-#include <linux/cpufreq.h> // cpufreq_get()
-#include <linux/cpumask.h> // cpumask_{first,next}(), cpu_online_mask
-
-#ifdef pr_fmt
-#undef pr_fmt
-#endif
-#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
-
-static int __init modinit(void)
-{
-        unsigned cpu = cpumask_first(cpu_online_mask);
-
-        while (cpu < nr_cpu_ids) {
-                seq_printf("CPU: %u, freq: %u kHz\n", cpu, cpufreq_get(cpu));
-                cpu = cpumask_next(cpu, cpu_online_mask);
-        }
-
-        return 0;
-}
-
-static void __exit modexit(void)
-{
-        // Empty function only to be able to unload the module.
-        return;
-}
-
-module_init(modinit);
-module_exit(modexit);
-MODULE_VERSION("0.1");
-MODULE_DESCRIPTION("Get CPU frequency for currently online CPUs.");
-MODULE_AUTHOR("Marco Bonelli");
 MODULE_LICENSE("GPL");

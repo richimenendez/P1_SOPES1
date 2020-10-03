906c1e28fa33b2915d26c96105ee61155c1bb3c3
richimenendez <richimenen@gmail.com>
2020-10-03 03:08:16 -0600

    test cpu2

--- ServerL/Procs/Cpu/cpu-module.c
+++ ServerL/Procs/Cpu/cpu-module.c
@@ -48,29 +48,6 @@ static int my_proc_show(struct seq_file *m, void *v)
 	seq_printf(m, "%d", totalpor);
 
 	return 0;
-}*/
-#include <linux/kernel.h>  // printk(), pr_*()
-#include <linux/module.h>  // THIS_MODULE, MODULE_VERSION, ...
-#include <linux/init.h>    // module_{init,exit}()
-#include <linux/smp.h>     // get_cpu(), put_cpu()
-#include <linux/cpufreq.h> // cpufreq_get()
-#include <linux/cpumask.h> // cpumask_{first,next}(), cpu_online_mask
-
-#ifdef pr_fmt
-#undef pr_fmt
-#endif
-#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
-
-static int my_proc_show(void)
-{
-        unsigned cpu = cpumask_first(cpu_online_mask);
-
-        while (cpu < nr_cpu_ids) {
-                pr_info("CPU: %u, freq: %u kHz\n", cpu, cpufreq_get(cpu));
-                cpu = cpumask_next(cpu, cpu_online_mask);
-        }
-
-        return 0;
 }
 
 static size_t my_proc_write(struct file *file, const char __user *buffer, size_t count, loff_t *f_pos)
@@ -108,11 +85,48 @@ static int __init test_init(void)
 
 static void __exit test_exit(void)
 {
-	//remove_proc_entry("cpu-module", NULL);
-	//printk(KERN_INFO "Final\n");
-	return;
-}
+	remove_proc_entry("cpu-module", NULL);
+	printk(KERN_INFO "Final\n");
+	}
 
 module_init(test_init);
 module_exit(test_exit);
+MODULE_LICENSE("GPL");
+*/
+// SPDX-License-Identifier: GPL-3.0
+#include <linux/kernel.h>  // printk(), pr_*()
+#include <linux/module.h>  // THIS_MODULE, MODULE_VERSION, ...
+#include <linux/init.h>    // module_{init,exit}()
+#include <linux/smp.h>     // get_cpu(), put_cpu()
+#include <linux/cpufreq.h> // cpufreq_get()
+#include <linux/cpumask.h> // cpumask_{first,next}(), cpu_online_mask
+
+#ifdef pr_fmt
+#undef pr_fmt
+#endif
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+static int __init modinit(void)
+{
+        unsigned cpu = cpumask_first(cpu_online_mask);
+
+        while (cpu < nr_cpu_ids) {
+                seq_printf("CPU: %u, freq: %u kHz\n", cpu, cpufreq_get(cpu));
+                cpu = cpumask_next(cpu, cpu_online_mask);
+        }
+
+        return 0;
+}
+
+static void __exit modexit(void)
+{
+        // Empty function only to be able to unload the module.
+        return;
+}
+
+module_init(modinit);
+module_exit(modexit);
+MODULE_VERSION("0.1");
+MODULE_DESCRIPTION("Get CPU frequency for currently online CPUs.");
+MODULE_AUTHOR("Marco Bonelli");
 MODULE_LICENSE("GPL");
